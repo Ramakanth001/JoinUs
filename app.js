f6f86138967fd3fe3f9b46c0e67fc98b98f366b7
Ramakanth001 <ramakanthseshabhattar@gmail.com>
2025-01-28 15:21:58 +0530

    code for entire flow

--- app.js
+++ app.js
@@ -2,14 +2,17 @@
 var express =  require('express');
 var app = express();
 
-// Used to export creds from envvironemnt
-require('dotenv').config();
-
 //used to connect to DB
 var mysql = require('mysql2'); 
 
-app.set("view engine", "ejs");
+// Use 
+var bodyParser = require("body-parser");
 
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Used to export creds from envvironment
+require('dotenv').config();
 
 var connection = mysql.createConnection({
   host: process.env.DB_HOST,
@@ -58,9 +61,22 @@ app.get("/", function(request, response) {
     });
   });
 
+app.post("/register", function(request, response){
+  console.log("POST REQUEST SENT TO " + request.body.email);
+  var person = {
+    email: request.body.email
+  };
+  connection.query('INSERT INTO users SET ?', person, function(error, result) {
+    if (error) throw error;
+    console.log(error);
+    console.log(result);
+    response.redirect("/");
+  });
+});
+
 app.listen(8080, function() {
   console.log("Listening on 8080 port!");
-})
+});
 
 
 // app.listen(8080, function(){
